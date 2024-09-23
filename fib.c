960ded20b7bafe83b7a5f032944be6035e3253ed
Thomas Nguyen <thomasn1003@csu.fullerton.edu>
2024-09-22 22:09:24 -0700

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,7 +1,66 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+
+typedef unsigned long long fib_type;
+
+fib_type iterative_fibonacci(int nthNum) {
+    fib_type prevprevnum = 0;
+    fib_type prevnum = 1;
+    fib_type currentnum = 0;
+
+    for (int i = 0; i < nthNum; i++) {
+        if (will_overflow(prevprevnum, prevnum)) {
+            printf("Overflow at Fibonacci index: %d\n", i);
+            exit(1);
+        }
+        prevprevnum = prevnum;
+        prevnum = currentnum;
+        currentnum = prevprevnum + prevnum;
+    }
+
+    return currentnum;
+}
+
+fib_type recursive_fibonacci(int nthNum) {
+    if (nthNum == 0) {
+        return 0;
+    } else if (nthNum == 1) {
+        return 1;
+    }
+    return recursive_fibonacci(nthNum - 1) + recursive_fibonacci(nthNum - 2);
+}
+
+
+
+
+int will_overflow(fib_type a, fib_type b) {
+    return a > 0 && b > 0 && (a > (ULLONG_MAX - b));
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+    int num = atoi(argv[1]) - 1;
+    char method = argv[2][0];
+    fib_type result;
+
+    clock_t start = clock();
+    
+    if (method == 'i') {
+        result = iterative_fibonacci(num);
+    } else if (method == 'r') {
+        result = recursive_fibonacci(num);
+    }
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+
+   printf("%lld\n", result);
+   //printf("Time taken: %f seconds\n", time_spent);
+
    return 0;
 }
